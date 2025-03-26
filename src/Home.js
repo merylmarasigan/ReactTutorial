7faca391ecd97b7eed18e175783024e4c0942ec8
Meryl Marasigan <meryl0729@gmail.com>
2025-03-26 15:26:34 -0700

    Lesson #12

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,14 @@ import PostsList from './PostsList';
 const Home = () => {
     const [blogPosts, setBlogs] = useState([
         {title:'My first blog post!', body:'lorem ipsum...', author:'mario', id:1},
-        {title:'Welcome party!', body:'lorem ipsum...', author:'yoshi', id:2}
+        {title:'Welcome party!', body:'lorem ipsum...', author:'yoshi', id:2},
+        {title:'Web dev tips', body:'lorem ipsum...', author:'mario', id:3}
     ])
 
     return (  
         <div className='home'>
             <PostsList posts={blogPosts} title='All Posts'></PostsList>
+            <PostsList posts={blogPosts.filter((post) => post.author === 'mario')} title="Mario' Posts"></PostsList>
         </div>
     );
 }
