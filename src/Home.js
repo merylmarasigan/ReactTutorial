0f27577247a0c141f8ad817bfccb5469e75e6d2e
Meryl Marasigan <meryl0729@gmail.com>
2025-03-25 17:05:41 -0700

    lesson #8

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import {useState} from 'react';
 const Home = () => {
+    const [name, setName] = useState('Meryl');
+    const [age,setAge] = useState(25);
+
+    const changeName = () => {
+        if(name === 'Meryl'){
+            setName('Aba');
+        }else{
+            setName('Meryl')
+        }
+
+        setAge(age + 1)
+    }
+
+
+
     return (  
         <div className='home'>
             <h2>Homepage</h2>
+            <p className='name'>{name} is {age} years old</p>
+            <button onClick={changeName}>Change name!</button>
         </div>
     );
 }
