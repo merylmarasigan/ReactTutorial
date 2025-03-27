f99c80e9bd672d01eb9d65731b7b66bc81e71085
Meryl Marasigan <meryl0729@gmail.com>
2025-03-27 14:53:57 -0700

    Lesson #18

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,31 @@ import {useState, useEffect} from 'react';
 import PostsList from './PostsList';
 const Home = () => {
     const [blogPosts, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
           .then(res => {
             return res.json();
           })
           .then(data => {
             setBlogs(data);
           })
+
+          setIsPending(false);
+            
+        }, 1000);
+        
       }, [])
     
 
     return (  
         <div className='home'>
+            {isPending && <div>Loading...</div>}
            { blogPosts && <PostsList posts={blogPosts} title='All Posts'></PostsList>}
+           
         </div>
     );
 }
