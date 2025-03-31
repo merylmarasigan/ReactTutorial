fec464a62e2d624af666a64c80979bdd83679316
Meryl Marasigan <meryl0729@gmail.com>
2025-03-31 16:01:54 -0700

    Lesson #26

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const PostDetails = () => {
     const { id } = useParams();
+    const {data: post, error, isPending} = useFetch("http://localhost:8000/blogs/" + id);
     return (
         <div className='post-details'>
-            <h2>Blog details - {id}</h2>
+            {isPending && <div>Loading... </div>}
+            {error && <div>{error} </div>}
+            {post && 
+            <article>
+                <h2>{post.title}</h2>
+                <p>Written by {post.author}</p>
+                <div>
+                    {post.body}
+                </div>
+            </article>}
         </div>
     );
 }
