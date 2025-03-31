784c6b6e4e6ef14bd5e0413783afbf25c908dec5
Meryl Marasigan <meryl0729@gmail.com>
2025-03-30 18:24:20 -0700

    Lesson #21

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // import logo from './logo.svg';
 import Navbar from './Navbar.js';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Home/>
-      </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Switch>
+            <Route path='/Home'>
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
+     </div>
+    </Router>
+    
   );
 }
 export default App;
