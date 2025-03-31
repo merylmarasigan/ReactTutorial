d78534e628f4491c620832cbc36a5dc9889b811d
Meryl Marasigan <meryl0729@gmail.com>
2025-03-31 15:05:56 -0700

    Lesson #24

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,9 +6,11 @@ const useFetch = (url) => {
     const [error,setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController(); 
+        // we can associate this abort controller with a specific fetch request and we can use it to stop the associated fetch request
 
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {signal: abortCont.signal}) // associating abort Controller with this fetch
                 .then(res => {
 
                     if(!res.ok){
@@ -22,11 +24,20 @@ const useFetch = (url) => {
                     setError(null);
                 })
             .catch(err => {
-                setError(err.message);
-                setIsPending(false);
+                //aborting a fetch throws an error, so we don't want to update state when an abort error is thrown
+                if (err.name === 'AbortError'){
+
+                    console.log('fetch aborted')
+                }else{
+                    setError(err.message);
+                    setIsPending(false);
+                }
+
             }) //catches the situation where we don't get a response
           
         }, 1000);
+
+        return () => {abortCont.abort();} //the cleanup method will call the abort function to stop the associated fetch
         
       }, [url])
 
