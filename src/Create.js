7dc0a5028bda2c43e3418a057d3fa7f3303b64f9
Meryl Marasigan <meryl0729@gmail.com>
2025-04-01 14:01:10 -0700

    Lesson #28

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
     return (  
         <div className="create">
             <h2>Add a New Blog Post</h2>
+            <form>
+                <label>Title:</label>
+                <input
+                    type='text'
+                    required
+                    value= {title}
+                    onChange = {(event) => {setTitle(event.target.value)}}
+                />
+
+                <label>Body:</label>
+                <textarea
+                required
+                value = {body}
+                onChange = {(event) => {setBody(event.target.value)}}
+                >
+                </textarea>
+
+                <label>Author:</label>
+                <select
+                    value = {author}
+                    onChange = {(event) => {setAuthor(event.target.value);}}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+
+                <button>Post</button>
+                <p>title: {title}</p>
+                <p>body: {body}</p>
+                <p>author: {author}</p>
+            </form>
         </div>
     );
 }
  
 export default Create;
+
+
+
