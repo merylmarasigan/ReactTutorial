24c83943d6dcad40de22802cecac82126f1e23bb
Meryl Marasigan <meryl0729@gmail.com>
2025-04-08 22:05:47 -0700

    when Delete is clicked, post is removed from supabase db and user is redirected to home page

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 // import { useState, useEffect } from "react"; // Import useEffect here
+import supabase from "./config/supabaseClient";
 
 
 const PostDetails = () => {
@@ -11,12 +12,22 @@ const PostDetails = () => {
     const { data: post, error, isPending } = useFetch(id);
     const history = useHistory();
 
-    const handleClick = () => {
-        fetch("http://localhost:8000/blogs/"+ id, {
-            method: "DELETE"
-        }).then(() => {
+    const handleClick = async () => {
+        // fetch("http://localhost:8000/blogs/"+ id, {
+        //     method: "DELETE"
+        // }).then(() => {
+        //     history.push('/');
+        // });
+
+        const { data, error } = await supabase.from("Posts").delete().eq('id',id).select();
+
+        if(error){
+            console.log(error);
+        }
+        if(data){
+            console.log(data)
             history.push('/');
-        });
+        }
     }
 
     return (
